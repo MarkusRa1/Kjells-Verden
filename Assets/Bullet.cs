72a0d1acc675372138c79bab0d0bb7e9b892906d
Markus Ragnhildstveit <markus.ragn@gmail.com>
2021-02-27 15:25:28 +0100

    la til skyting

---
+++ Assets/Bullet.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    void TheStart(Transform enemyContainer)
+    {
+        Transform enemy = FindClosestChild(enemyContainer);
+        if (enemy != null)
+        {
+            Vector2 direction = enemy.position - transform.position;
+            rb.velocity = direction/direction.magnitude * speed;
+            Destroy(gameObject, 10f);
+        } 
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    Transform FindClosestChild(Transform enemyContainer)
+    {
+        if (enemyContainer.childCount == 0)
+            return null;
+
+        Transform closestEnemy = enemyContainer.GetChild(0);
+        float closestEnemyMagnitude = (transform.position - closestEnemy.position).magnitude;
+
+        foreach (Transform enemy in enemyContainer)
+        {
+            float enemyMagnitude = (transform.position - enemy.position).magnitude;
+            if (enemyMagnitude < closestEnemyMagnitude)
+            {
+                closestEnemy = enemy;
+                closestEnemyMagnitude = enemyMagnitude;
+            }
+        }
+        return closestEnemy;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+
+    }
+}
