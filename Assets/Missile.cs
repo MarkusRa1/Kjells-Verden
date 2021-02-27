082a81967c4799621a9fa483f72c8ff1b51dca26
Markus Ragnhildstveit <markus.ragn@gmail.com>
2021-02-27 18:31:21 +0100

    la til missile

--- Assets/Missile.cs
+++ Assets/Missile.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int damage = 100;
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    private Transform enemyContainer1;
+
+    void TheStart(Transform enemyContain)
     {
-        
+        enemyContainer1 = enemyContain;
+        Transform target = FindClosestChild(enemyContainer1);
+
+        if (target != null)
+        {
+            Vector2 direction = target.position - transform.position;
+            rb.velocity = direction / direction.magnitude * speed;
+            Destroy(gameObject, 20f);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Transform target = FindClosestChild(enemyContainer1);
+
+        if (target != null)
+        {
+            Vector2 direction = target.position - transform.position;
+            rb.velocity = direction / direction.magnitude * speed;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
+    Transform FindClosestChild(Transform enemyContainer)
+    {
+        if (enemyContainer.childCount == 0)
+            return null;
+
+        Transform closestEnemy = enemyContainer.GetChild(0);
+        Vector2 bulletPos = transform.position;
+        Vector2 enemyPos = closestEnemy.position;
+        float closestEnemyMagnitude = (bulletPos - enemyPos).magnitude;
+
+        foreach (Transform enemy in enemyContainer)
+        {
+            enemyPos = enemy.position;
+            float enemyMagnitude = (bulletPos - enemyPos).magnitude;
+            if (enemyMagnitude < closestEnemyMagnitude)
+            {
+                closestEnemy = enemy;
+                closestEnemyMagnitude = enemyMagnitude;
+            }
+        }
+        return closestEnemy;
+    }
+
+
+    // Virker ikke!!!!
+    //void OnTriggerEnter2D(Collider2D hitInfo)
+    //{
+    //    Debug.Log(hitInfo.name);
+    //
+    //    Enemy enemy = hitInfo.GetComponent<Enemy>();
+    //    //Debug.Log()
+    //    if (enemy != null)
+    //    {
+    //        enemy.TakeDamage(damage);
+    //        Destroy(gameObject);
+    //    }
+    //}
 }
